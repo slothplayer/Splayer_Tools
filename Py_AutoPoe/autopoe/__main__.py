b9213555e16bfa76428989748aaae2d5321e0838
Splayer <sean@DSean.local>
2022-09-07 17:43:06 +0800

    AutoPoe undone

--- Py_AutoPoe/autopoe/__main__.py
+++ Py_AutoPoe/autopoe/__main__.py
@@ -2,16 +2,27 @@ import pyautogui as pag
 import time
 
 # pag.PAUSE = 2
-pag.FAILSAFE = False
+# pag.FAILSAFE = False
 fps_60 = 60/1000
+mid_x = 735
+mid_y = 500
+
+
+anchor = [mid_x, mid_y, 0, 0]
 
 def test():
+	parameter = [-200, -200, 200, 200]
+	targer_pic = []
+	for i in range(len(anchor)):
+		targer_pic.append(anchor[i]+parameter[i]) 
+	print(targer_pic)
 	# while(1):
 	# 	print(pag.position())
 	# 	time.sleep(fps_60)
 
-	temp = pag.screenshot()
-	temp.save(r'./data/temp.jpg')
+	# temp = pag.screenshot()
+	temp = pag.screenshot(imageFilename = r'./data/temp1.png', region = targer_pic)
+	# temp.save(r'./data/temp.png')
 
 def main():
 	test()
